5711ce42a2fbf88486d4cb05b613c4136267980d
Brandon Machida <brandon.machida@gmail.com>
2024-11-07 13:40:50 -1000

    Update 07_monthly_trend_by_delay_type.sql

--- sql-queries/07_monthly_trend_by_delay_type.sql
+++ sql-queries/07_monthly_trend_by_delay_type.sql
@@ -8,7 +8,7 @@ SELECT
     SUM(delay_security) AS security_delay_duration,
     SUM(delay_late_aircraft_arrival) AS late_aircraft_delay_duration
 FROM 
-    all_airlines_departure_data_hnl
+    alaska_airlines_departure_data_hnl
 GROUP BY 
     carrier_code, STRFTIME('%Y-%m', date)
 ORDER BY 
