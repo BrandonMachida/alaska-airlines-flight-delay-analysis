18918dd9d6400790e534542f6b7744189df8bb4c
Brandon Machida <brandon.machida@gmail.com>
2024-11-07 13:38:23 -1000

    Update 02_delay_type_breakdown.sql

--- sql-queries/02_delay_type_breakdown.sql
+++ sql-queries/02_delay_type_breakdown.sql
@@ -3,7 +3,7 @@ SELECT
     carrier_code,
     'Carrier' AS delay_type,
     AVG(delay_carrier) AS average_delay_minutes
-FROM all_airlines_departure_data_hnl
+FROM alaska_airlines_departure_data_hnl
 WHERE 
     departure_delay >= 15
 GROUP BY
@@ -15,7 +15,7 @@ SELECT
     carrier_code,
     'Weather' AS delay_type,
     AVG(delay_weather) AS average_delay_minutes
-FROM all_airlines_departure_data_hnl
+FROM alaska_airlines_departure_data_hnl
 WHERE 
     departure_delay >= 15
 GROUP BY
@@ -27,7 +27,7 @@ SELECT
     carrier_code,
     'National Aviation System' AS delay_type,
     AVG(delay_national_aviation_system) AS average_delay_minutes
-FROM all_airlines_departure_data_hnl
+FROM alaska_airlines_departure_data_hnl
 WHERE 
     departure_delay >= 15
 GROUP BY
@@ -39,7 +39,7 @@ SELECT
     carrier_code,
     'Security' AS delay_type,
     AVG(delay_security) AS average_delay_minutes
-FROM all_airlines_departure_data_hnl
+FROM alaska_airlines_departure_data_hnl
 WHERE 
     departure_delay >= 15
 GROUP BY
@@ -51,7 +51,7 @@ SELECT
     carrier_code,
     'Late Aircraft Arrival' AS delay_type,
     AVG(delay_late_aircraft_arrival) AS average_delay_minutes
-FROM all_airlines_departure_data_hnl
+FROM alaska_airlines_departure_data_hnl
 WHERE 
     departure_delay >= 15
 GROUP BY
